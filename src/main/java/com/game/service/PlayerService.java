98cde4dd9bafcdba118f094d967047f3a3073529
Yury Kuzma <vterkin1388@gmail.com>
2022-09-20 22:07:28 +0300

    javaRush getRestPlayers

---
+++ src/main/java/com/game/service/PlayerService.java
@@ -0,0 +1,25 @@
+package com.game.service;
+
+import com.game.entity.Player;
+import com.game.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> findAll() {
+        return playerRepository.findAll();
+    }
+}
